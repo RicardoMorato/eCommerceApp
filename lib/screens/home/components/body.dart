cf34976ce4973c0390bd1917c557edfae5464299
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-25 23:59:02 -0300

    feat(product-image): Finished first image's container.

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/Products.dart';
 
 import 'categories.dart';
 
@@ -20,9 +21,31 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
-        Container(
-          height: 180,
-          width: 160,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(kDefaultPadding),
+              height: 180,
+              width: 160,
+              decoration: BoxDecoration(
+                color: products[0].color,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Image.asset(products[0].image),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPadding / 4),
+          child: Text(
+            products[0].title,
+            style: TextStyle(color: kTextLightColor),
+          ),
+        ),
+        Text(
+          '\$234',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ],
     );
