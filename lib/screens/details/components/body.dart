bc0ca7f4e279aa2e90e7f10b9699025aef1366bb
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-26 11:08:42 -0300

    feat(details-screen): Added color check boxes.

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/Products.dart';
 import 'package:shop_app/screens/details/components/product_title_with_image.dart';
 
@@ -19,7 +20,11 @@ class Body extends StatelessWidget {
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.4),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
@@ -27,12 +32,73 @@ class Body extends StatelessWidget {
                       topRight: Radius.circular(24),
                     ),
                   ),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Color'),
+                              Row(
+                                children: <Widget>[
+                                  ColorDot(
+                                    color: Color(0xFF356C95),
+                                    isSelected: true,
+                                  ),
+                                  ColorDot(
+                                    color: Color(0xFFF8C078),
+                                  ),
+                                  ColorDot(
+                                    color: Color(0xFFA29B9B),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 ProductTitleWithImage(product: product),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+
+  const ColorDot({Key key, this.color, this.isSelected = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: kDefaultPadding / 4,
+        right: kDefaultPadding / 2,
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
       ),
     );
   }
