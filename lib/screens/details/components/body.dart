172069ee5ab8bfbfac6cac4d113faa0d284aabb4
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-26 00:38:27 -0300

    feat(product-details): Added price to Details Screen.

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -45,6 +45,27 @@ class Body extends StatelessWidget {
                               fontWeight: FontWeight.bold,
                             ),
                       ),
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(text: 'Price\n'),
+                                TextSpan(
+                                  text: '\$${product.price}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline4
+                                      .copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ],
                   ),
                 ),
