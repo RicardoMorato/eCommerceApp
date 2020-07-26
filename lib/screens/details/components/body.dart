fb299d548d8ed4b447a89d11beac4903a5270d61
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-26 11:16:43 -0300

    feat(details-screen): Finished color check boxes and size.

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -3,6 +3,8 @@ import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/Products.dart';
 import 'package:shop_app/screens/details/components/product_title_with_image.dart';
 
+import 'color_and_size.dart';
+
 class Body extends StatelessWidget {
   final Product product;
 
@@ -34,29 +36,8 @@ class Body extends StatelessWidget {
                   ),
                   child: Column(
                     children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text('Color'),
-                              Row(
-                                children: <Widget>[
-                                  ColorDot(
-                                    color: Color(0xFF356C95),
-                                    isSelected: true,
-                                  ),
-                                  ColorDot(
-                                    color: Color(0xFFF8C078),
-                                  ),
-                                  ColorDot(
-                                    color: Color(0xFFA29B9B),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
+                      ColorAndSize(
+                        product: product,
                       ),
                     ],
                   ),
@@ -66,39 +47,6 @@ class Body extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  final Color color;
-  final bool isSelected;
-
-  const ColorDot({Key key, this.color, this.isSelected = false})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: kDefaultPadding / 4,
-        right: kDefaultPadding / 2,
-      ),
-      padding: EdgeInsets.all(2.5),
-      height: 24,
-      width: 24,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: isSelected ? color : Colors.transparent,
-        ),
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
       ),
     );
   }
